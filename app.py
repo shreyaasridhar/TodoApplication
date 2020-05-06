3f6fc95533bd2fe9de61a5c4b93866935fc7e971
shreyaasridhar <shreyanju@gmail.com>
2020-05-06 06:46:26 -0700

    Created a basic todo with refresh

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)  # name the application app after the file
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://shreyaasridhar@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    __tablename__ = "todos"
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo {self.id}, {self.description}>'
+
+
+db.create_all()
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=[{
-        'description': 'Todo1'
-    }, {
-        'description': 'Todo2'
-    }, {
-        'description': 'Todo3'
-    }])
+    return render_template('index.html', data=Todo.query.all())
+
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    desc = request.form.get("description", '')
+    todo = Todo(description=desc)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
