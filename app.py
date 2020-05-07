1ee15d29b80f70544c4592d33a593b69a9422902
shreyaasridhar <shreyanju@gmail.com>
2020-05-07 06:23:27 -0700

    Created a syncronous JS call with fetch

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
+import sys
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)  # name the application app after the file
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://shreyaasridhar@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 
 class Todo(db.Model):
     __tablename__ = "todos"
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
-        return f'<Todo {self.id}, {self.description}>'
-
-
-db.create_all()
+        return f'< Todo {self.id}, {self.description}>'
 
 
 @app.route('/')
@@ -26,8 +28,21 @@ def index():
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    desc = request.form.get("description", '')
-    todo = Todo(description=desc)
-    db.session.add(todo)
-    db.session.commit()
-    return redirect(url_for('index'))
+    error = False
+    body = {}
+    try:
+        desc = request.get_json()['description']
+        todo = Todo(description=desc)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        return abort(400)
+    else:
+        return jsonify(body)
